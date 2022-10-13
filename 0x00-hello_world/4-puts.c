71b9a2bd6180c73b363bd9c8f1633286d0a1245c
soabolaji <soabolaji@gmail.com>
2022-10-13 17:07:01 +0100

    Multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-	int main() {
-	
+	int main(void)
+	{
 /*initialising strings*/
-
-	char str[] = "Programming is like building a multilingual puzzle\n";
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
 
 /*Printing a standard output*/
-
 	puts(str);
 
-	return 0;
+	return (0);
 	}
