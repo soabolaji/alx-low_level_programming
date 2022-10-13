b292b95cff48b53eaa3c959b677295d190dc2b62
soabolaji <soabolaji@gmail.com>
2022-10-14 00:00:22 +0100

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'that piece of art is useful'
+ *
+ * Return:  1 if success
+ *
+ */
+
+	int main(void)
+	{
+write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+	}
