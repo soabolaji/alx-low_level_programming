27f25dd9e8ba635da5feb284097865ec7d571722
soabolaji <soabolaji@gmail.com>
2022-10-14 21:04:34 +0100

    Big combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,14 +19,13 @@
 
 	for (x = 0; x <= 98; x++)
 	{
-	for (z = x + 0; z <= 99; z++)
+	for (z = x + 1; z <= 99; z++)
 	{
 	putchar((x / 10) + '0');
 	putchar((x % 10) + '0');
 	putchar(' ');
 	putchar((z / 10) + '0');
 	putchar((z % 10) + '0');
-	
 	if (x == 98 && z == 99)
 	continue;
 
