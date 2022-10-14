212b7186bc799a25d695686b67edbaa541169737
soabolaji <soabolaji@gmail.com>
2022-10-14 09:35:30 +0100

    Random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print positive or negative number'
+ *
+ * Return: Always 0 ( success)
+ *
+ */
+
+	int main()
+	
+	{
+	
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	
+	else if (n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+
+	else 
+	{
+	printf("%d is negative\n", n);
+	}
+	
+	return (0);
+	
+
+	}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Conditional statements in C programming(If...else, for loop, while and do/while).
