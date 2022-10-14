1deb12770a09f7a209b93ad10c30c3325a700e7e
soabolaji <soabolaji@gmail.com>
2022-10-14 17:31:17 +0100

    10digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print base 10 digits'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+
+	int main(void)
+	{
+	int z;
+	for (z = 0; z < 10; z++)
+	putchar((z % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+	}
