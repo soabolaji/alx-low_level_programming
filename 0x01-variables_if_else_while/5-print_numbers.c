014618b8e87e3414b63509f561d21deee2050af9
soabolaji <soabolaji@gmail.com>
2022-10-14 16:43:29 +0100

    Base 10 digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print base digit'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+
+	int main(void)
+	{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	printf("%d", x);
+
+	printf("\n");
+
+	return (0);
+	}
