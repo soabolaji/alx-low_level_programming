f181c118f58998796325f82eea5fea5340bfc4b5
soabolaji <soabolaji@gmail.com>
2022-10-18 21:35:33 +0100

    Multiple of natural number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 
 
 /**
- * print_to_98 - Prints all natural numbers from input to 98, separated by a comma followed by a space.
+ * print_to_98 - Prints all natural numbers from input to 98
  *
  * @n: The number to begin counting at.
  *
