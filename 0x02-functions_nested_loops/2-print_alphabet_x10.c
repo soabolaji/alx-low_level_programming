bc5300dedc10eab8951427a9176c1d82f3b4684f
soabolaji <soabolaji@gmail.com>
2022-10-18 16:53:10 +0100

    Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10: 'Print alphabet 10 times'
+ *
+ */
+
+
+
+	void print_alphabet_x10(void)
+	{
+	int alph_x10 = 0;
+	int alph;
+
+	while (alph_x10++ <= 9)
+	{
+	
+	for (alph = 'a'; alph <= 'z'; alph++)
+	_putchar(alph);
+
+
+	_putchar('\n');
+
+	}
+
+	}
