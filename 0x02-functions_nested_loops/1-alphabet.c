e51da96f34652c6b9555dc482e748bebd7de3c57
soabolaji <soabolaji@gmail.com>
2022-10-18 16:12:06 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+ * main - entry point
+ *
+ * Description: ' Print alphabet in lower case'
+ *
+ * Return: alwsys
+ *
+ */
+
+	void print_alphabet(void)
+	{
+	
+	char alph;
+
+	for ( alph = 'a'; alph < 'z'; alph++)
+	_putchar(alph);
+
+
+	_putchar('\n');
+
+	}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-/*#ifndef MAIN_H
-#define MAIN_H
-*/
 
 /**
  * File: main.h
@@ -11,7 +8,7 @@
  */
 
 	int _putchar(char c);
-/*	void print_alphabet(void);
+	void print_alphabet(void);
 	void print_alphabet_x10(void);
 	int _islower(int c);
 	int _isalpha(int c);
@@ -24,5 +21,3 @@
 	void print_to_98(int n);
 	void print_times_table(int n);
 
-#endif
-*/
