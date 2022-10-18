11ac8e2dee4edffac4861085391e4c0fe0e48fcf
soabolaji <soabolaji@gmail.com>
2022-10-18 22:15:38 +0100

    Multiple numbers

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+
+
+/**
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
+ *
+ */
+
+
+	void print_times_table(int n)
+	{
+	int num;
+	int multiple;
+	int result;
+
+	if (n >= 0 && n <= 15)
+	{
+	for (num = 0; num <= n; num++)
+	{
+	_putchar('0');
+
+	for (multiple = 1; multiple <= n; multiple++)
+	{
+	_putchar(',');
+	_putchar(' ');
+
+	result = multiple * num;
+	
+	if (result <= 99)
+	_putchar(' ');
+
+	if (result <= 9)
+	_putchar(' ');
+
+	if (result >= 100)
+	{
+	_putchar((result / 100) + '0');
+	_putchar((result / 10) % 10 + '0');
+	}
+	else if (result <= 99 && result >= 10)
+	{
+	_putchar((result / 10) + '0');
+	}
+	_putchar((result % 10) + '0')
+	
+	}
+	_putchar('\n');
+	}
+	}
+	}
