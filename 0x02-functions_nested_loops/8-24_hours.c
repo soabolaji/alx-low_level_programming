742237c9de67eaa4f292b1291616fad65ce96ddf
soabolaji <soabolaji@gmail.com>
2022-10-18 20:20:46 +0100

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,12 +18,11 @@
 	{
 	for (minute = 0; minute <= 59; minute++)
 	{
-	
-	_putchar(hour / 10);
-	_putchar(hour % 10);
+	_putchar((hour / 10) + '0');
+	_putchar((hour % 10) + '0');
 	_putchar(':');
-	_putchar(minute / 10);
-	_putchar(minute % 10);
+	_putchar((minute / 10) + '0');
+	_putchar((minute % 10) + '0');
 	_putchar('\n');
 	}
 	}
