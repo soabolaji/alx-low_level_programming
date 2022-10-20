14792d38434933be6c170548fdc8944a55cce33f
soabolaji <soabolaji@gmail.com>
2022-10-20 06:28:48 +0100

    Debugging file

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,7 +10,7 @@
   * Return: void
   */
 
-	int print_remaining_days(int month, int day, int year)
+	void print_remaining_days(int month, int day, int year)
 	{
 	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 	{
