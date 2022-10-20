8e46b14a307fa7bba09a68345df950d48eb1ee49
soabolaji <soabolaji@gmail.com>
2022-10-20 13:48:55 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ *
+ * Return: always 0.
+ *
+ */
+
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 100; num++)
+	{
+	if ((num % 3) == 0 && (num % 5) == 0)
+	printf("FizzBuzz");
+
+	else if ((num % 3) == 0)
+	printf("Fizz");
+
+	else if ((num % 5) == 0)
+	printf("Buzz");
+
+	else
+	printf("%d", num);
+
+	if (num == 100)
+	continue;
+
+	printf(" ");
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
