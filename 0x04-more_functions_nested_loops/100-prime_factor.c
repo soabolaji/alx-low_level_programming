7dd2bcd115b79efa2e6a6097f46a6d39f7f49197
soabolaji <soabolaji@gmail.com>
2022-10-20 14:50:54 -0700

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - entry point
+ *
+ *
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	long prime = 612852475143, div;
+
+	while (div < (prime / 2))
+	{
+	if ((prime % 2) == 0)
+	{
+	prime /= 2;
+	continue;
+	}
+
+	for (div = 3; div < (prime / 2); div += 2)
+	{
+	if ((prime % div) == 0)
+	prime /= div;
+	}
+	}
+	printf("%ld\n", prime);
+
+	return (0);
+}
