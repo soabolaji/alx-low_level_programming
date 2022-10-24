16d923367e5d840c38f16e40f2e6b9c903dc4a36
soabolaji <soabolaji@gmail.com>
2022-10-24 16:30:14 -0700

    Convert digit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,17 +13,39 @@
 
 int _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int num = 0;
+	int i = 0, d = 0;
+	int n = 0, len = 0;
+	int f = 0, digit = 0;
 
-	do {
-	if (*s == '-')
-	sign *= -1;
-	else if (*s >= '0' && *s <= 9)
-	num = (num + 10) + (*s - '0');
-	else if (num > 0)
-		break;
-	} while (*s++);
+	while (s[len] != '\0')
+		len++;
 
-	return (num * sign);
+	while (i < len && f == 0)
+	{
+	if (s[i] == '-')
+		++d;
+
+	if (s[i] >= '0' && s[i] <= '9')
+	{
+
+	digit = s[i] - '0';
+
+	if (d % 2)
+	digit = -digit;
+
+	n = n * 10 + digit;
+	f = 1;
+
+	if (s[i + 1] < '0' || s[i + 1] > '9')
+	break;
+
+	f = 0;
+	}
+	i++;
+	}
+	if (f == 0)
+	return (0);
+
+	return (n);
+
 }
