264c4aa59ad39ab08d4dedc7308c020fa0322e33
soabolaji <soabolaji@gmail.com>
2022-10-24 09:04:44 -0700

    the string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+
+/**
+ * _puts - Print string to the standard output.
+ *
+ * @str: pointer variable to the string.
+ *
+ */
+
+void _puts(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+	_putchar(str[n]);
+
+	}
+
+	_putchar('\n');
+
+}
+
+	str
