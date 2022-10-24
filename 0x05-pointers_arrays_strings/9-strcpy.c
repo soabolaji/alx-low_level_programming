d6fc085781a85cf4f7f07a7d7c9f3ead2f62f9c6
soabolaji <soabolaji@gmail.com>
2022-10-24 14:21:08 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _strcpy - function that copies the string pointed to by src,
+ *
+ * @dest: pointed to buffer.
+ *
+ * @src: the termination
+ *
+ * Return: Always point the destination.
+ */
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int nex = 0;
+
+	while (src[nex])
+	{
+	dest[nex] = src[nex];
+	nex++;
+	}
+
+	return (dest);
+
+}
