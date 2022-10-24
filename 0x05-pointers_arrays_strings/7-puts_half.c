45672a0553e568549ac62e163687203eb9da7ceb
soabolaji <soabolaji@gmail.com>
2022-10-24 13:12:44 -0700

    Two Points

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+/**
+ * puts_half - function that prints half of a strin.
+ *
+ * @str: the string
+ *
+ */
+
+
+void puts_half(char *str)
+{
+	int index = 0;
+	int lenth = 0;
+	int i;
+
+	while (str[index++])
+	lenth++;
+
+	if ((lenth % 2) == 0)
+	i = lenth / 2;
+
+	else
+
+	i = (lenth + 1) / 2;
+
+	for (index = i; index < lenth; index)
+	_putchar(str[index]);
+
+	_putchar('\n');
+
+}
