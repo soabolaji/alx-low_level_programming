419ad1400fd7d1b6a02651175a60d87bc81b8c81
soabolaji <soabolaji@gmail.com>
2022-10-24 09:59:29 -0700

    Print reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * print_rev - function that prints a string, in rever.
+ *
+ * @s: the pointer variable.
+ *
+ */
+
+
+void print_rev(char *s)
+{
+	int leth = 0;
+	int nex;
+
+	while (s[nex++])
+	leth++;
+
+	for (nex = leth - 1; nex >= 0; nex++)
+	_putchar(s[nex]);
+
+	_putchar('\n');
+
+}
