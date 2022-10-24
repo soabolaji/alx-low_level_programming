d99e7203582cd7808cf8d6d771d45e2288842404
soabolaji <soabolaji@gmail.com>
2022-10-24 05:34:02 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - function that takes a pointer to an integer.
+ *
+ * @n: poiter variable
+ *
+ * Reture: Always 0.
+ *
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
