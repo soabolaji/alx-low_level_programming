c72e87f30786f595bcfcccdef114f467771ea8f9
soabolaji <soabolaji@gmail.com>
2022-10-27 06:08:43 -0700

    infinite number

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,51 +6,50 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 
 /**
- * add_strings - Adds the numbers stored in two strings.
- * @n1: The string containing the first number to be added.
- * @n2: The string containing the second number to be added.
+ * add_strings - Adds the two strings.
+ * @n1: The string containing the first number.
+ * @n2: The string containing the second number.
  * @r: The buffer to store the result.
  * @r_index: The current index of the buffer.
  *
- * Return: If r can store the sum - a pointer to the result.
- *         If r cannot store the sum - 0.
+ * Return: 0 If r can store the sum - a pointer to the result.
  */
 
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
-	int num, tens = 0;
+	int number, count = 0;
 
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
-	num = (*n1 - '0') + (*n2 - '0');
-	num += tens;
-	*(r + r_index) = (num % 10) + '0';
-	tens = num / 10;
+	number = (*n1 - '0') + (*n2 - '0');
+	number += count;
+	*(r + r_index) = (number % 10) + '0';
+	count = number / 10;
 
 	}
 
 	for (; *n1; n1--, r_index--)
 	{
-	num = (*n1 - '0') + tens;
-	*(r + r_index) = (num % 10) + '0';
-	tens = num / 10;
+	number = (*n1 - '0') + count;
+	*(r + r_index) = (number % 10) + '0';
+	count = number / 10;
 	}
 
 	for (; *n2; n2--, r_index--)
 	{
-	num = (*n2 - '0') + tens;
-	*(r + r_index) = (num % 10) + '0';
-	tens = num / 10;
+	number = (*n2 - '0') + count;
+	*(r + r_index) = (number % 10) + '0';
+	count = number / 10;
 	}
 
 
-	if (tens && r_index >= 0)
+	if (count && r_index >= 0)
 	{
-	*(r + r_index) = (tens % 10) + '0';
+	*(r + r_index) = (count % 10) + '0';
 	return (r + r_index);
 	}
 
-	else if (tens && r_index < 0)
+	else if (count && r_index < 0)
 	return (0);
 
 	return (r + r_index + 1);
@@ -72,18 +71,18 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int index, n1_len = 0, n2_len = 0;
+	int index, n1_lenth = 0, n2_lenth = 0;
 
 	for (index = 0; *(n1 + index); index++)
-	n1_len++;
+	n1_lenth++;
 
 	for (index = 0; *(n2 + index); index++)
-	n2_len++;
-	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+	n2_lenth++;
+	if (size_r <= n1_lenth + 1 || size_r <= n2_lenth + 1)
 	return (0);
 
-	n1 += n1_len - 1;
-	n2 += n2_len - 1;
+	n1 += n1_lenth - 1;
+	n2 += n2_lenth - 1;
 	*(r + size_r) = '\0';
 	return (add_strings(n1, n2, r, --size_r));
 }
