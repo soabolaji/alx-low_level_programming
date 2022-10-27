20fc1b23a59e6b67dca544bd7f895bdd65e1da26
soabolaji <soabolaji@gmail.com>
2022-10-26 22:09:22 -0700

    Reverse array int

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -18,8 +18,10 @@ void reverse_array(int *a, int n)
 	int rev, index;
 
 	for (index = n - 1; index >= n / 2; index--)
+	{
 	rev = a[n - 1 - index];
 	a[n - 1 - index] = a[index];
 	a[index] = rev;
+	}
 
 }
