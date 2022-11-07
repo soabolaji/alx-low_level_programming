f828fb5ce4c894c259b00256bf577d4b6ecd0e06
soabolaji <soabolaji@gmail.com>
2022-11-07 13:20:03 -0800

    fixed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,33 +14,36 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **iarray;
-	int i, n;
+	int **twoD;
+	int hgt_index, wid_index;
 
 	if (width <= 0 || height <= 0)
 	return (NULL);
 
-	iarray = malloc(sizeof(int) * height);
+	twoD = malloc(sizeof(int *) * height);
 
-	if (iarray == NULL)
+	if (twoD == NULL)
+	return (NULL);
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-	free(iarray);
+	twoD[hgt_index] = malloc(sizeof(int) * width);
+
+	if (twoD[hgt_index] == NULL)
+	{
+	for (; hgt_index >= 0; hgt_index--)
+	free(twoD[hgt_index]);
+
+	free(twoD);
 	return (NULL);
 	}
-
-	for (i = 0; i < height; i++)
-	{
-	iarray[i] = malloc(sizeof(int) * width);
-	if (iarray == NULL)
-	{
-	free(iarray);
-	return (NULL);
 	}
 
-	for (n = 0; n < width; n++)
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-	iarray[i][n] = 0;
+	for (wid_index = 0; wid_index < width; wid_index++)
+	twoD[hgt_index][wid_index] = 0;
 	}
-	}
-	return (iarray);
+
+	return (twoD);
 }
