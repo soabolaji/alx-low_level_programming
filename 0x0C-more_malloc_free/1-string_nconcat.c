f7dc05c2191065f68c3b6d44ed052598129f0024
soabolaji <soabolaji@gmail.com>
2022-11-10 12:52:07 -0800

    fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,39 +13,40 @@
 
 char *string_nmemcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	unsigned int i;
+	unsigned int j;
+	unsigned int s1_len;
+	unsigned int concat_len;
+	char *concat;
+
+	s1_len = 0;
 
 	if (s1 == NULL)
-	s1 = "";
+		s1 = "";
+
 	if (s2 == NULL)
-	s2 = "";
+		s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i]; i++)
+		s1_len++;
+
+	concat_len = s1_len + n;
+
+	concat = malloc(sizeof(char) * (concat_len + 1));
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < s1_len; i++)
 	{
+		concat[i] = s1[i];
 	}
 
-	for (j = 0; s2[j] != '\0'; j++)
+	for (j = 0; j < n; j++, i++)
 	{
+		concat[i] = s2[j];
 	}
 
-	if (n < j)
-	j = n;
+	concat[i] = '\0';
 
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
-	return (NULL);
-
-	for (co = 0; co < i; co++)
-	arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
-	{
-	arr[co] = s2[co_2];
-	co++;
-	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	return (concat);
 }
