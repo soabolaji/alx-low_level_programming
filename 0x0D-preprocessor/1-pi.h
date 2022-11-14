3c79bef787881fc7fbeaccb91933aaafca2de551
soabolaji <soabolaji@gmail.com>
2022-11-14 08:50:48 -0800

    pi

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -3,7 +3,7 @@
 
 
 /**
- * Desc: header file that defines a macro named PI as an
+ * PI - header file that defines a macro named PI as an
  * abbreviation for the token 3.14159265359.
  * Return: Always 0
  */
