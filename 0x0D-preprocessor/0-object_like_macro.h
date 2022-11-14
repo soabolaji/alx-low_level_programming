27ff8ea6e823665462fc077432ef97468425713d
soabolaji <soabolaji@gmail.com>
2022-11-14 08:47:56 -0800

    object

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,7 +3,7 @@
 
 
 /**
- * Desc: header file that defines a macro named SIZE as an
+ * SIZE - header file that defines a macro named SIZE as an
  * abbreviation for the token 1024.
  * Return: Always 0
  */
