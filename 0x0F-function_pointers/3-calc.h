55a54b0610f49d6dc7e995a84b6dcad4c49a311a
soabolaji <soabolaji@gmail.com>
2022-11-16 12:46:12 -0800

    fpointer

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,21 +1,17 @@
 #ifndef CALC_H
 #define CALC_H
 
-/**
- * Desc: programme that perform simple operation
- *
- */
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op - A structure type
+ * @op: the operator
+ * @f: the associate function
  */
+
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
