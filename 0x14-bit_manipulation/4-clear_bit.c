3d9fa8531d6d45aed4c4fca44bbe01b5d39740aa
soabolaji <soabolaji@gmail.com>
2022-12-01 10:08:43 -0800

    bitwise

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,11 +9,8 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8)
-			return (-1);
-
-			*n &= ~(1 << index);
-
-			return (1);
-
+	if (index > ((sizeof(*n) * 8) - 1))
+	return (-1);
+	*n &= ~(1 << index);
+	return (1);
 }
